53b7699ed3f16f157a047edef6594488a6aec4ec
Anastasiya Olshanskaya <shulz-86@yandex.ru>
2023-03-01 15:12:30 +0300

    added project

---
+++ src/test/java/demoqa/TestBase.java
@@ -0,0 +1,15 @@
+package demoqa;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+
+public class TestBase {
+    RegistrationPage registrationPage = new RegistrationPage();
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.holdBrowserOpen = true;
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+    }
+}
