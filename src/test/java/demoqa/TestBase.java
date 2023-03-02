12e8dae86bcf803dbc6eca83d27057f5b8ed2b39
Anastasiya Olshanskaya <shulz-86@yandex.ru>
2023-03-02 10:17:26 +0300

    added attachments

--- src/test/java/demoqa/TestBase.java
+++ src/test/java/demoqa/TestBase.java
@@ -2,10 +2,12 @@ package demoqa;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class TestBase {
     RegistrationPage registrationPage = new RegistrationPage();
@@ -17,6 +19,11 @@ public class TestBase {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+
     }
 
     @BeforeEach
@@ -26,6 +33,9 @@ public class TestBase {
 
     @AfterEach
     void addAttachments() {
-
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 }
